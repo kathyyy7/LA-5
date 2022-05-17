91182d812a56a96f010ef86b8244578419f8af88
Kathy Franco <kfrancoba@mail.fresnostate.edu>
2022-05-17 12:47:46 -0700

    structured english

---
+++ lab_5.py
@@ -0,0 +1,21 @@
+
+"""""
+The program is trying to translate a sentence captured as user input.
+We first need to read in the text file textese.txt
+Then from the text file, we first create a dictionary from the text file.
+Then, we create a dictionary the list.
+Once the text file has been read into memory, we close the file.
+
+We then define a function for translating which evolves splitting the user input (sentence)
+into an array of strings ("enjoy the excellent band tonight") ["enjoy", "the", "excellent", "band", "tonight]
+
+Once we have the array of strings representing the user's input sentence, we loop through
+each words, find the key matching the word and returns back the value tied to the word
+in our dictionary.
+
+After each word is translated, we then
+Print out the translated sentence to the user.
+
+"""""
+
+
